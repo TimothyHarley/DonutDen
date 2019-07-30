68f0772f1e46e05ed952d09d21c2a2277bfd2a86
Tim Harley <Timothy.D.Harley@gmail.com>
2019-07-30 16:51:37 -0500

    styled menu, added filler pic and paragraph

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -10,19 +10,22 @@ class MenuItem extends React.Component {
   render(){
     const { MenuItem } = this.props;
     return(
-      <div className="menuItem-box">
-        <Row>
-          <Col>
-            <img src={logo} className="menuItemImage" alt={MenuItem.name} />
-          </Col>
-          <Col>
+      <Row className="containerRow">
+        <Col className="imageCol">
+          <img src={logo} className="menuItemImage" alt={MenuItem.name} />
+        </Col>
+        <Row className="dataRow">
+          <Col className="itemName">
             <h5>{MenuItem.name}</h5>
           </Col>
-          <Col>
-            {MenuItem.price}
+          <Col className="itemPrice">
+            ${MenuItem.price}
           </Col>
+          <Row className="itemDescription">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet massa a est porttitor facilisis eget in leo. Phasellus quis nisl dolor. Vestibulum cursus sem rutrum turpis mollis, a placerat ante commodo.</p>
+          </Row>
         </Row>
-      </div>
+      </Row>
     );
   }
 }
