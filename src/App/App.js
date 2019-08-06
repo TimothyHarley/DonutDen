81c16eb845dd3709a6bcacf7f67cebb40906931d
Tim Harley <Timothy.D.Harley@gmail.com>
2019-08-06 16:32:29 -0500

    fixed auth bug

--- src/App/App.js
+++ src/App/App.js
@@ -20,7 +20,7 @@ import './App.scss';
 const PublicRoute = ({ component: Component, authed, ...rest }) => {
   const routeChecker = props => (authed === false
     ? (<Component { ...props } />)
-    : (<Redirect to={{ pathname: '/home', state: { from: props.location } }} />));
+    : (<Redirect to={{ pathname: '/employees', state: { from: props.location } }} />));
   return <Route {...rest} render={props => routeChecker(props)} />;
 };
 
@@ -71,10 +71,10 @@ class App extends React.Component {
             <div className="App-container">
               <div className="justify-content-center">
                 <Switch>
-                  <PublicRoute path='/' exact component={Home} authed={this.state.authed} />
-                  <PublicRoute path='/home' component={Home} authed={this.state.authed} />
-                  <PublicRoute path='/menu' component={Menu} authed={this.state.authed} />
-                  <PublicRoute path='/order' component={Order} authed={this.state.authed} />
+                  <Route path='/' exact component={Home} authed={this.state.authed} />
+                  <Route path='/home' component={Home} authed={this.state.authed} />
+                  <Route path='/menu' component={Menu} authed={this.state.authed} />
+                  <Route path='/order' component={Order} authed={this.state.authed} />
                   <PublicRoute path='/auth' component={Auth} authed={this.state.authed} />
                   <PrivateRoute path='/employees' component={Employees} authed={this.state.authed} />
                 </Switch>
