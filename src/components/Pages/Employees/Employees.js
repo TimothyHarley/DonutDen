a2a9ac7035cf3a0c619c348bc1903d02eb12b850
Tim Harley <Timothy.D.Harley@gmail.com>
2019-08-06 19:03:59 -0500

    added date search bar

--- src/components/Pages/Employees/Employees.js
+++ src/components/Pages/Employees/Employees.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, Form, FormGroup, Label, Input, Row, Col } from 'reactstrap';
 import './Employees.scss'
 
 class Employees extends React.Component {
@@ -6,6 +7,21 @@ class Employees extends React.Component {
     return(
       <div>
         <h1>Employees</h1>
+        <Form>
+          <Row>
+            <Col md={2}>
+              <FormGroup>
+                <Label for="exampleDate">Search Orders By Date</Label>
+                <Input
+                  type="date"
+                  name="date"
+                  id="exampleDate"
+                  placeholder="date placeholder"
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+        </Form>
       </div>
     );
   }
