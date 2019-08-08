aaa6e3bbb61d905d8892a884dfa83589c9c43865
Tim Harley <Timothy.D.Harley@gmail.com>
2019-08-08 07:38:31 -0500

    added some more home screen

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import logo from '../../../images/DonutDenLogo.jpeg';
+import { Row, Col } from 'reactstrap';
 import './Home.scss'
 
 class Home extends React.Component {
@@ -28,6 +30,22 @@ class Home extends React.Component {
             <div className="neon-text">FOX'S DONUT DEN</div>
           </div>
         </div>
+        <Row className="about-us">
+          <Col md={5} className="logo-box">
+            <img src={logo} alt="logo" />
+          </Col>
+          <Col md={5} className="info">
+            <p>The Donut Den has been a Nashville tradition since 1973.
+            It's difficult to remember a time before that big neon sign started lighting 
+            up Green Hills at night, reminding us that our friendly neighborhood donut shop
+            was still there.  With it's mom-and-pop atmosphere, and it's daily-fresh donuts, it's the 
+            perfect way to lessen the stress of that Hillsboro Pike commute.  So come on in!
+            If we don't have what you want today, we'll gladly save one for you tomorrow! 
+            </p>
+          </Col>
+        </Row>
+        
+        
       </div>
     );
   }
